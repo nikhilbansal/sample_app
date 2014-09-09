f4c85f746703aa867753cddfbab96091b69247c0
Nikhil Bansal <nikhilbansi@gmail.com>
2014-09-09 13:11:57 +0530

    Learned a little more about ruby

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
+  render_views
+
+  before(:each) do
+    @base_title = "Some Ruby Tutorials Title"
+  end
 
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    it "should have the right title" do
+      get :home
+    end
   end
 
   describe "GET contact" do
     it "returns http success" do
       get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe "GET about" do
+    it "returns http success" do
+      get :about
       expect(response).to have_http_status(:success)
     end
   end
